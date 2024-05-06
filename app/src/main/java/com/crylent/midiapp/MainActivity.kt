e5f8356907e981dec44e06ef00be29c2961b52c0
crylent Maxim Belkovsky <inkerimaa1@gmail.com>
2024-05-06 12:44:36 +0300

    Demo app update: filter type spinner

--- app/src/main/java/com/crylent/midiapp/MainActivity.kt
+++ app/src/main/java/com/crylent/midiapp/MainActivity.kt
@@ -1,13 +1,19 @@
 package com.crylent.midiapp
 
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.AdapterView.OnItemSelectedListener
+import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.Spinner
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.crylent.midilib.AudioEngine
+import com.crylent.midilib.soundfx.Filter
 import com.google.android.material.switchmaterial.SwitchMaterial
 
 class MainActivity : AppCompatActivity() {
@@ -36,6 +42,32 @@ class MainActivity : AppCompatActivity() {
         findViewById<SwitchMaterial>(R.id.filterSwitch).apply {
             setOnCheckedChangeListener { _, checked ->
                 Midi.filter.enabled = checked
+            }
+        }
+
+        val filters = mapOf(
+            "Low Pass" to Filter.Type.LowPass,
+            "High Pass" to Filter.Type.HighPass,
+            "Band Pass" to Filter.Type.BandPass,
+            "Band Stop" to Filter.Type.BandStop
+        )
+        findViewById<Spinner>(R.id.filterSpinner).apply {
+            adapter = ArrayAdapter(
+                this@MainActivity,
+                android.R.layout.simple_spinner_dropdown_item,
+                filters.keys.toList()
+            )
+            onItemSelectedListener = object : OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    position: Int,
+                    id: Long
+                ) {
+                    Midi.filter.type = filters.values.toList()[position]
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) {}
             }
         }
     }
