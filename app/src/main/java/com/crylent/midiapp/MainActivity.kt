80d1679edb976cd49b29d5cd9d8cca12de00389c
crylent Maxim Belkovsky <inkerimaa1@gmail.com>
2024-05-06 16:54:01 +0300

    Demo app update: ADSR controls

--- app/src/main/java/com/crylent/midiapp/MainActivity.kt
+++ app/src/main/java/com/crylent/midiapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.crylent.midiapp
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.AdapterView
 import android.widget.AdapterView.OnItemSelectedListener
@@ -9,13 +10,16 @@ import android.widget.Button
 import android.widget.SeekBar
 import android.widget.Spinner
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.IdRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.crylent.midilib.AudioEngine
+import com.crylent.midilib.envelope.ADSREnvelope
 import com.crylent.midilib.soundfx.Filter
 import com.google.android.material.switchmaterial.SwitchMaterial
+import kotlin.math.pow
 import kotlin.math.roundToInt
 
 class MainActivity : AppCompatActivity() {
@@ -53,6 +57,16 @@ class MainActivity : AppCompatActivity() {
                 AudioEngine.allNotesOff(0)
             }
         }
+
+        val envelope = Midi.instrument.envelope as ADSREnvelope
+        val attack = paramToProgress(envelope.attack)
+        val decay = paramToProgress(envelope.decay)
+        Log.d("midilib", decay.toString())
+        val release = paramToProgress(envelope.release)
+        createEnvelopeParam(R.id.attack, attack) { envelope.attack = progressToParam(it) }
+        createEnvelopeParam(R.id.decay, decay) { envelope.decay = progressToParam(it) }
+        createEnvelopeParam(R.id.sustain, envelope.sustain) { envelope.sustain = it }
+        createEnvelopeParam(R.id.release, release) { envelope.release = progressToParam(it) }
 
         findViewById<SwitchMaterial>(R.id.filterSwitch).apply {
             setOnCheckedChangeListener { _, checked ->
@@ -110,5 +124,32 @@ class MainActivity : AppCompatActivity() {
                 override fun onStopTrackingTouch(seekBar: SeekBar?) {}
             })
         }
+    }
+
+    private fun paramToProgress(value: Float) = (value / MULTIPLIER).pow(1f / POW)
+    private fun progressToParam(progress: Float) = MULTIPLIER * progress.pow(POW)
+
+    private fun createEnvelopeParam(@IdRes id: Int, value: Float, lambda: (value: Float)->Unit) {
+        findViewById<SeekBar>(id).apply {
+            progress = (value * max).roundToInt()
+            setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+                override fun onProgressChanged(
+                    seekBar: SeekBar?,
+                    progress: Int,
+                    fromUser: Boolean
+                ) {}
+
+                override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+
+                override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                    lambda(progress.toFloat() / max)
+                }
+            })
+        }
+    }
+
+    companion object {
+        private const val POW = 3f
+        private const val MULTIPLIER = 10f
     }
 }
