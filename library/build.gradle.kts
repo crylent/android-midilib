6728211eca91244f69fda227864c8ce7bdc32bcf
crylent Maxim Belkovsky <inkerimaa1@gmail.com>
2024-06-04 17:47:16 +0300

    version 2.0.1

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -11,7 +11,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.crylent"
             artifactId = "midilib"
-            version = "2.0"
+            version = "2.0.1"
 
             afterEvaluate {
                 from(components["release"])
